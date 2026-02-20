a10421171d5be92ce591744c6a01f3dd889ddcb7
deniedhash <sinhaanimesh345@gmail.com>
2026-02-20 19:48:06 +0530

    test

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const mongoose = require('mongoose');
-const { token, mongoUri } = require('./config');
-const commandHandler = require('./src/handlers/commandHandler');
-const eventHandler = require('./src/handlers/eventHandler');
-const { startWebhookServer } = require('./src/github/webhookServer');
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const mongoose = require("mongoose");
+const { token, mongoUri } = require("./config");
+const commandHandler = require("./src/handlers/commandHandler");
+const eventHandler = require("./src/handlers/eventHandler");
+const { startWebhookServer } = require("./src/github/webhookServer");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildVoiceStates,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
 });
+
+//test
 
 client.commands = new Collection();
 
 commandHandler(client);
 eventHandler(client);
 
-mongoose.connect(mongoUri).then(() => {
-    console.log('Connected to MongoDB');
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("Connected to MongoDB");
     return client.login(token);
-}).then(() => {
+  })
+  .then(() => {
     startWebhookServer(client);
-}).catch(err => {
-    console.error('Startup error:', err);
+  })
+  .catch((err) => {
+    console.error("Startup error:", err);
     process.exit(1);
-});
+  });
